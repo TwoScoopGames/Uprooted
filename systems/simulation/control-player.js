664a02a28655dae0ec17a37b5661324b0287c949
Eric Lathrop <eric@ericlathrop.com>
2015-04-17 23:12:26 -0400

    Add slamming.

--- systems/simulation/control-player.js
+++ systems/simulation/control-player.js
@@ -2,8 +2,6 @@
 
 module.exports = function(ecs, data) {
 	ecs.addEach(function(entity, elapsed) { // jshint ignore:line
-		entity.velocity.y += 0.01; // gravity
-
 		if (entity.position.y > 400) { // floor
 			entity.position.y = 400;
 			entity.velocity.y = 0;
@@ -11,7 +9,12 @@ module.exports = function(ecs, data) {
 		}
 
 		if (data.input.button("left")) {
-			entity.charge.left += elapsed;
+			if (entity.velocity.y != 0) {
+				entity.velocity.x = 0;
+				entity.velocity.y = 1.5;
+			} else {
+				entity.charge.left += elapsed;
+			}
 		} else if (entity.charge.left > 0) {
 			console.log("release left", entity.charge.left);
 			entity.velocity.y = -1;
@@ -19,12 +22,20 @@ module.exports = function(ecs, data) {
 			entity.charge.left = 0;
 		}
 		if (data.input.button("right")) {
-			entity.charge.right += elapsed;
+			if (entity.velocity.y != 0) {
+				entity.velocity.x = 0;
+				entity.velocity.y = 1.5;
+			} else {
+				entity.charge.right += elapsed;
+			}
 		} else if (entity.charge.right > 0) {
 			console.log("release right", entity.charge.right);
 			entity.velocity.y = -1;
 			entity.velocity.x = entity.charge.right / 1000;
 			entity.charge.right = 0;
 		}
+
+		entity.velocity.y += 0.01; // gravity
+
 	}, ["player"]);
 };
