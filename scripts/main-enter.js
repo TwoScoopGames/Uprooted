51e1638f7cb66d89c5ca9eb0768a82f3a3988d2d
Eric Lathrop <eric@ericlathrop.com>
2015-04-18 17:47:00 -0400

    Start adding trim.

--- scripts/main-enter.js
+++ scripts/main-enter.js
@@ -1,9 +1,10 @@
 "use strict";
 
 var tileSize = 64;
-function makeBlock(entityPool, name, x, y) {
+
+function makeTrim(entityPool, name, x, y) {
 	var entity = entityPool.add();
-	entity.position = { x: x, y: y };
+	entity.position = { x: x * tileSize, y: y * tileSize };
 	entity.size = { width: tileSize, height: tileSize };
 	entity.image = {
 		"name": name,
@@ -16,14 +17,21 @@ function makeBlock(entityPool, name, x, y) {
 		"destinationWidth": tileSize,
 		"destinationHeight": tileSize
 	};
+	return entity;
+}
+
+function makeBlock(entityPool, name, x, y) {
+	var entity = makeTrim(entityPool, name, x, y);
 	entity.collisions = [];
 }
 
 
 module.exports = function(data) {
 	var landscapes = [ "-", "-", "-", "/", "\\", " "];
-	var col = 0;
+	var col = 1;
 	var row = 0;
+	var cols = [null];
+
 	while (col < 200) {
 		var type = landscapes[Math.floor(Math.random() * landscapes.length)];
 		if (col < 10) {
@@ -33,23 +41,43 @@ module.exports = function(data) {
 			var len = Math.floor(Math.random() * 10) + 1;
 
 			for (var i = 0; i < len; i++) {
-				makeBlock(data.entities, "ground11", (col + i) * tileSize, (row * tileSize));
+				cols.push({type: type, row: row});
 			}
 			col += len;
 		}
 		if (type === " ") {
 			var len = Math.floor(Math.random() * 5) + 1;
+			for (var i = 0; i < len; i++) {
+				cols.push(null);
+			}
 			col += len;
 		}
 		if (type === "/") {
 			row--;
-			makeBlock(data.entities, "ground11", col * tileSize, (row * tileSize));
+			cols.push({type: type, row: row});
 			col++;
 		}
 		if (type === "\\") {
+			cols.push({type: type, row: row});
 			row++;
-			makeBlock(data.entities, "ground11", col * tileSize, (row * tileSize));
 			col++;
+		}
+	}
+
+	var sprites = {
+		"-": ["ground8", "ground9", "ground10", "ground11"],
+		"/": ["ground2"],
+		"\\": ["ground4"]
+	};
+	for (var col = 0; col < cols.length; col++) {
+		var c = cols[col];
+		if (c !== null) {
+			var possibleSprites = sprites[c.type];
+			var sprite = possibleSprites[Math.floor(Math.random() * possibleSprites.length)];
+			if (c.type === "-") {
+				makeTrim(data.entities, "ground5", col, c.row - 1);
+			}
+			makeBlock(data.entities, sprite, col, c.row);
 		}
 	}
 };
