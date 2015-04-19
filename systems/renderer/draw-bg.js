60aa8d153b81ccaf721c708ba1d945880f58d0b5
Eric Lathrop <eric@ericlathrop.com>
2015-04-19 16:01:06 -0400

    Fix crash when there's no camera.

--- systems/renderer/draw-bg.js
+++ systems/renderer/draw-bg.js
@@ -3,11 +3,15 @@
 module.exports = function(ecs, data) {
 	ecs.add(function() {
 		var bg = data.images.get("background");
-		if (bg) {
-			var camera = data.entities.entities[2];
-			var ypct = Math.max(Math.min((camera.position.y + 500) / 1000.0, 1), 0);
-			var bgy = -640 * ypct;
-			data.context.drawImage(bg, 0, bgy);
+		if (!bg) {
+			return;
 		}
+		var camera = data.entities.entities[2];
+		if (!camera) {
+			return;
+		}
+		var ypct = Math.max(Math.min((camera.position.y + 500) / 1000.0, 1), 0);
+		var bgy = -640 * ypct;
+		data.context.drawImage(bg, 0, bgy);
 	});
 };
