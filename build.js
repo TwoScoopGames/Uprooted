d2527577f19f19a9c4f6e7b4a6220bda74774a5b
Eric Lathrop <eric@ericlathrop.com>
2015-07-19 16:10:29 -0400

    Update build script for new systems file format

--- build.js
+++ build.js
@@ -11,16 +11,16 @@ scripts.forEach(function(script) {
 
 var systems = require("./systems");
 systems.simulation.forEach(function(system) {
-	if (system.indexOf("splatjs:") === 0) {
+	if (system.name.indexOf("splatjs:") === 0) {
 		return;
 	}
-	b.require(system);
+	b.require(system.name);
 });
 systems.renderer.forEach(function(system) {
-	if (system.indexOf("splatjs:") === 0) {
+	if (system.name.indexOf("splatjs:") === 0) {
 		return;
 	}
-	b.require(system);
+	b.require(system.name);
 });
 
 var out = fs.createWriteStream("./index.js");
